fd93d70484318b9da1ee038afdf7c5c902c05a2a
Zhuofeng Wang <zhuofengwang2003@gmail.com>
2025-05-09 20:35:51 +0800

    Init commit.

---
+++ recipes/openeb/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cmake -B build -S . \
+  -DCMAKE_INSTALL_PREFIX=$PREFIX \
+  -DPYTHON3_SITE_PACKAGES=$SP_DIR \
+  -DPython3_EXECUTABLE=$PYTHON \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DBUILD_TESTING=ON \
+  -DCOMPILE_PYTHON3_BINDINGS=ON \
+  -DUDEV_RULES_SYSTEM_INSTALL=OFF \
+  -DCMAKE_SYSROOT=$BUILD_PREFIX/$HOST/sysroot \
+  ${CMAKE_ARGS} \
+  -Wno-dev
+
+cmake --build build --target install -j
